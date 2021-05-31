f2be6b8e0937bead257333a95cf6500484c45e94
Joshua Fletcher <joshua.f1998@gmail.com>
2021-05-31 19:49:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const append = pets.push('Odie')
+const petsArray = ["Milo", "Otis", "Garfield"]
+
+const append = petsArray
+
+const prepend = petsArray
+
+const removeLast = petsArray
+
+const RemoveFirst = petsArray
